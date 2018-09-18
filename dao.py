5609447c434da92ccefb4b0693499074da95d8e3
Aluno <adamascenosj@gmail.com>
2018-09-18 16:03:06 -0300

    Update: add dao.py

--- dao.py
+++ dao.py
@@ -3,9 +3,13 @@ from model import Animal, Cliente, Consulta
 
 db = dataset.connect('sqlite:///database.db')
 
-class Dao(object):
+class DaoAnimal(object):
 
 
     @staticmethod
-    def funcname(parameter_list):
-        pass
+    def retornar_todos():
+        return [Animal(nome_dono=registro['nome_dono'],tipo_animal=registro['tipo_animal'], raca=registro['raca'], id=registro['id']) for registro in db['animal'].all()]
+
+    @staticmethod
+    def inserir_animal(animal):
+        return db['animal'].insert(dict(nome_dono=animal.nome_dono, tipo_animal=animal.tipo_animal, raca=animal.raca))
